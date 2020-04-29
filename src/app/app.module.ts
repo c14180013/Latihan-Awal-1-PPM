b76c01755c22696d24ba448281d56675c0d1e679
c14180013 <c14180013@john.petra.ac.id>
2020-04-29 08:06:24 +0700

    3 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HasilComponent } from './hasil/hasil.component';
+
+const ROUTES = [
+  {path: 'kalkulator', component : AppComponent},
+  {path: 'hasil', component : HasilComponent},
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES) ],
+  declarations: [ AppComponent, HelloComponent, HasilComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
